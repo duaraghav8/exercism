2f8c746a3d4ddcd6041194cca0a201ec87a73c80
duaraghav8 <12758282+duaraghav8@users.noreply.github.com>
2020-09-12 19:39:30 +0530

    go react: callback logic

--- go/react/react.go
+++ go/react/react.go
@@ -1,9 +1,10 @@
 package react
 
 type cell struct {
-	r         *reactor
-	value     int
-	onRefresh func()
+	r                 *reactor
+	value             int
+	onRefresh         func()
+	onUpdateCallbacks []*callback
 }
 
 func (c *cell) Value() int {
@@ -11,6 +12,9 @@ func (c *cell) Value() int {
 }
 
 func (c *cell) setValue(v int) {
+	if c.value == v {
+		return
+	}
 	c.value = v
 	c.r.refresh()
 }
@@ -23,12 +27,29 @@ func (ic *inputCell) SetValue(v int) {
 	ic.setValue(v)
 }
 
+type callback struct {
+	f       func(int)
+	enabled bool
+}
+
+func (c *callback) Cancel() {
+	c.enabled = false
+}
+
+func (c *callback) exec(i int) {
+	if c.enabled {
+		c.f(i)
+	}
+}
+
 type computeCell struct {
 	*cell
 }
 
-func (c *computeCell) AddCallback(cb func(int)) Canceler {
-	return nil
+func (c *computeCell) AddCallback(f func(int)) Canceler {
+	cb := &callback{f: f, enabled: true}
+	c.onUpdateCallbacks = append(c.onUpdateCallbacks, cb)
+	return cb
 }
 
 type reactor struct {
@@ -37,9 +58,10 @@ type reactor struct {
 
 func (r *reactor) CreateInput(i int) InputCell {
 	c := &cell{
-		r:         r,
-		value:     i,
-		onRefresh: func() {},
+		r:                 r,
+		value:             i,
+		onRefresh:         func() {},
+		onUpdateCallbacks: make([]*callback, 0, 0),
 	}
 
 	r.trackCell(c)
@@ -47,25 +69,36 @@ func (r *reactor) CreateInput(i int) InputCell {
 }
 
 func (r *reactor) CreateCompute1(c Cell, f func(int) int) ComputeCell {
-	cc := &cell{r: r}
-	cc.onRefresh = func() {
-		cc.value = f(c.Value())
+	newCell := &cell{
+		r:                 r,
+		onUpdateCallbacks: make([]*callback, 0, 0),
 	}
-	cc.onRefresh()
+	newCell.onRefresh = func() {
+		newCell.value = f(c.Value())
 
-	r.trackCell(cc)
-	return &computeCell{cc}
+		for _, cb := range newCell.onUpdateCallbacks {
+			cb.exec(newCell.value)
+		}
+	}
+	newCell.onRefresh()
+
+	r.trackCell(newCell)
+	return &computeCell{newCell}
 }
 
 func (r *reactor) CreateCompute2(c1 Cell, c2 Cell, f func(int, int) int) ComputeCell {
-	cc := &cell{r: r}
-	cc.onRefresh = func() {
-		cc.value = f(c1.Value(), c2.Value())
-	}
-	cc.onRefresh()
+	newCell := &cell{r: r}
+	newCell.onRefresh = func() {
+		newCell.value = f(c1.Value(), c2.Value())
 
-	r.trackCell(cc)
-	return &computeCell{cc}
+		for _, cb := range newCell.onUpdateCallbacks {
+			cb.exec(newCell.value)
+		}
+	}
+	newCell.onRefresh()
+
+	r.trackCell(newCell)
+	return &computeCell{newCell}
 }
 
 func (r *reactor) trackCell(c *cell) {
@@ -83,3 +116,7 @@ func (r *reactor) refresh() {
 func New() Reactor {
 	return &reactor{cells: make([]*cell, 0, 0)}
 }
+
+// todo
+// - onRefresh() functions currently compute the value everytime. Call them only if cell's value actually changes
+// - refactor: move repeated code into a common place
